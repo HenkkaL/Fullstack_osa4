8b932f2bd17417c048ad32a7a62fa6e27704d4c0
Henrik Lyra <henrik.lyra@stat.fi>
2018-05-23 21:10:30 +0300

    4.2 blogilista, osa 2

--- blogilista/index.js
+++ blogilista/index.js
@@ -1,21 +1,12 @@
-const http = require('http')
 const express = require('express')
+const blogsRouter = require('./controllers/blogs')
 const app = express()
-const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-module.exports = Blog
-
+app.use('/api/blogs', blogsRouter)
 app.use(cors())
-app.use(bodyParser.json())
 
 if ( process.env.NODE_ENV !== 'production' ) {
   require('dotenv').config()
@@ -23,34 +14,6 @@ if ( process.env.NODE_ENV !== 'production' ) {
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
-
-const formatBlog = (blog) => {
-    return {
-        title: blog.title,
-        author: blog.author,
-        url: blog.url,
-        likes: blog.likes,
-        id: blog._id
-    }
-}
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs.map(formatBlog))
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
