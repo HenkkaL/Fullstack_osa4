965196ea54e350caa2e5f055b626f8a0f6a46f74
Henrik Lyra <henrik.lyra@stat.fi>
2018-05-24 21:55:12 +0300

    4.5* apufunktioita ja yksikkötestejä, osa 3

--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -5,14 +5,23 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
     if (blogs.length === 0)
         return 0
-    const reducer = (accumulator, currentValue) => {
-         
+    const reducer = (accumulator, currentValue) => {         
         return accumulator + currentValue.likes
     }
 
     return blogs.reduce(reducer, 0)    
   }
   
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0)
+        return null
+    const reducer = (max, item) => {
+        return item.likes > max.likes ? item : max
+    }
+
+    return blogs.reduce(reducer, blogs[0])
+  }
+
   module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
   }
