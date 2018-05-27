cc26275b1fbf13dfde62900194c3d33f41324ef5
Henrik Lyra <henriklyra@gmail.com>
2018-05-27 16:11:04 +0300

    4.10* blogilistan testit, osa 3

--- blogilista/controllers/blogs.js
+++ blogilista/controllers/blogs.js
@@ -2,29 +2,40 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 const formatBlog = (blog) => {
-    return {
-        title: blog.title,
-        author: blog.author,
-        url: blog.url,
-        likes: blog.likes,
-        id: blog._id
-    }
+  return {
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes,
+    id: blog._id
+  }
 }
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+  const blogs = await Blog.find({})
 
-    response.json(blogs.map(formatBlog))
-  })
+  response.json(blogs.map(formatBlog))
+})
 
-  blogsRouter.post('', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
+
+blogsRouter.post('', async (request, response) => {
+  try {
+    const body = request.body
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes === undefined ? 0 : body.likes
+    })
+
+    const savedBlog = await blog.save()
+    response.json(formatBlog(blog))
+
+  } catch (exception) {
+    console.log(exception)
+    response.status(500).json({ error: 'something went wrong... ' })
+  }
+})
 
 module.exports = blogsRouter
--- blogilista/tests/blog_api.test.js
+++ blogilista/tests/blog_api.test.js
@@ -11,6 +11,41 @@ test('all blogs are returned as json by GET /api/blogs', async () => {
         .expect('Content-Type', /application\/json/)
 })
 
+
+
+test('new blog can be added ', async () => {
+    const newBlog = {
+        "title": "testitestiTesti",
+        "author": "author",
+        "url": "urli",
+        "likes": 4
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    
+})
+
+test('new blog with undefined likes can be added ', async () => {
+    const newBlog = {
+        "title": "testitestiTesti",
+        "author": "author",
+        "url": "urli"
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    
+})
+
+
 afterAll(() => {
     server.close()
 })
+
