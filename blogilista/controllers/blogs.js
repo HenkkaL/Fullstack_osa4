f913e9494f6f23b1918d02009dda06577baa9248
Henrik Lyra <henriklyra@gmail.com>
2018-05-27 17:58:45 +0300

    4.14* blogilistan laajennus, osa 3

--- blogilista/controllers/blogs.js
+++ blogilista/controllers/blogs.js
@@ -42,6 +42,25 @@ blogsRouter.post('', async (request, response) => {
   }
 })
 
+blogsRouter.put('/:id', async (request, response) => {
+  try {
+    const body = request.body
+
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+    }
+  
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+        response.json(formatBlog(updatedBlog))
+    } catch (exception) {
+    console.log(exception)
+    response.status(500).json({ error: 'something went wrong...' })
+  }
+})
+
 blogsRouter.delete('/:id', async (request, response) => {
   try {
     await Blog.findByIdAndRemove(request.params.id)
