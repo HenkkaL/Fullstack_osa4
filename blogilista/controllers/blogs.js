f07af9aa19da203d413d7e4e6f379b5b6b9636df
Henrik Lyra <henriklyra@gmail.com>
2018-05-27 21:51:31 +0300

    4.17 blogilistan laajennus, osa 6

--- blogilista/controllers/blogs.js
+++ blogilista/controllers/blogs.js
@@ -1,20 +1,13 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-
-const formatBlog = (blog) => {
-  return {
-    title: blog.title,
-    author: blog.author,
-    url: blog.url,
-    likes: blog.likes,
-    id: blog._id
-  }
-}
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user', { name: 1, username: 1 } )
 
-  response.json(blogs.map(formatBlog))
+  response.json(blogs.map(Blog.format))
 })
 
 
@@ -26,16 +19,23 @@ blogsRouter.post('', async (request, response) => {
       return response.status(400).json({ error: 'content missing' })
     }
 
+    const users = await User.find({})
+    const user = users[0]
+
     const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
-      likes: body.likes === undefined ? 0 : body.likes
+      likes: body.likes === undefined ? 0 : body.likes,
+      user: user._id
     })
 
     const savedBlog = await blog.save()
-    response.json(formatBlog(blog))
 
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.json(Blog.format(blog))
   } catch (exception) {
     console.log(exception)
     response.status(500).json({ error: 'something went wrong... ' })
@@ -54,7 +54,7 @@ blogsRouter.put('/:id', async (request, response) => {
     }
   
     const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-        response.json(formatBlog(updatedBlog))
+        response.json(Blog.format(updatedBlog))
     } catch (exception) {
     console.log(exception)
     response.status(500).json({ error: 'something went wrong...' })
