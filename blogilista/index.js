e6a20d48972b8ab1f6f86f07730fd001dce879a8
Henrik Lyra <henrik.lyra@stat.fi>
2018-05-28 21:48:39 +0300

    4.20* blogilistan laajennus, osa 9

--- blogilista/index.js
+++ blogilista/index.js
@@ -23,12 +23,14 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(express.static('build'))
 app.use(middleware.logger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
 app.use(middleware.error)
+
 
 const server = http.createServer(app)
 
--- blogilista/utils/middleware.js
+++ blogilista/utils/middleware.js
@@ -1,7 +1,7 @@
 const logger = (request, response, next) => {
     if ( process.env.NODE_ENV === 'test' ) {
         return next()
-      }
+    }
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
     console.log('Body:  ', request.body)
@@ -13,7 +13,21 @@ const error = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const getTokenFrom = (request) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
+
+const tokenExtractor = (request, responce, next) => {
+    request.token = getTokenFrom(request)
+    next()
+}
+
 module.exports = {
     logger,
-    error
+    error,
+    tokenExtractor
 }
