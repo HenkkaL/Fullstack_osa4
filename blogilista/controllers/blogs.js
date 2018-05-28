36f6fe14060e26ae49fdfb2f97f33ed7cce173af
Henrik Lyra <henrik.lyra@stat.fi>
2018-05-28 21:16:17 +0300

    4.19 blogilistan laajennus, osa 8

--- blogilista/controllers/blogs.js
+++ blogilista/controllers/blogs.js
@@ -1,75 +1,93 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog
-    .find({})
-    .populate('user', { name: 1, username: 1 } )
+    const blogs = await Blog
+        .find({})
+        .populate('user', { name: 1, username: 1 } )
 
-  response.json(blogs.map(Blog.format))
+    response.json(blogs.map(Blog.format))
 })
 
+const getTokenFrom = (request) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 blogsRouter.post('', async (request, response) => {
-  try {
     const body = request.body
 
-    if (body.title === undefined || body.url === undefined ) {
-      return response.status(400).json({ error: 'content missing' })
+    try {
+        const token = getTokenFrom(request)
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+
+        if (!token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' })
+        }
+
+        if (body.title === undefined || body.url === undefined ) {
+            return response.status(400).json({ error: 'content missing' })
+        }
+
+        const user = await User.findById(decodedToken.id)
+
+        const blog = new Blog({
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes === undefined ? 0 : body.likes,
+            user: user._id
+        })
+
+        const savedBlog = await blog.save()
+
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+
+        response.json(Blog.format(blog))
+    } catch (exception) {
+        if (exception.name === 'JsonWebTokenError' ) {
+            response.status(401).json({ error: exception.message })
+        } else {
+            console.log(exception)
+            response.status(500).json({ error: 'something went wrong... ' })
+        }
     }
-
-    const users = await User.find({})
-    const user = users[0]
-
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes === undefined ? 0 : body.likes,
-      user: user._id
-    })
-
-    const savedBlog = await blog.save()
-
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-
-    response.json(Blog.format(blog))
-  } catch (exception) {
-    console.log(exception)
-    response.status(500).json({ error: 'something went wrong... ' })
-  }
 })
 
 blogsRouter.put('/:id', async (request, response) => {
-  try {
-    const body = request.body
+    try {
+        const body = request.body
 
-    const blog = {
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes
-    }
-  
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+        const blog = {
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes
+        }
+
+        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
         response.json(Blog.format(updatedBlog))
     } catch (exception) {
-    console.log(exception)
-    response.status(500).json({ error: 'something went wrong...' })
-  }
+        console.log(exception)
+        response.status(500).json({ error: 'something went wrong...' })
+    }
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  try {
-    await Blog.findByIdAndRemove(request.params.id)
+    try {
+        await Blog.findByIdAndRemove(request.params.id)
 
-    response.status(204).end()
-  } catch (exception) {
-    console.log(exception)
-    response.status(400).send({ error: 'malformatted id' })
-  }
+        response.status(204).end()
+    } catch (exception) {
+        console.log(exception)
+        response.status(400).send({ error: 'malformatted id' })
+    }
 })
 
 module.exports = blogsRouter
